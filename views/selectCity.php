257a3b163e497d897aa15655f12a12313e9dea49
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-03-11 15:46:47 +0200

    adding views and styles

---
+++ views/selectCity.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SelectCity</title>
+    <link rel="stylesheet" href="styles/selectCity.css">
+</head>
+<body>
+    <main class="container">
+        <div>
+            <select id="myTextId" name="" id="">
+                <option value="-" selected>-- City --</option>
+                <option value=""></option>
+            </select>
+            <button type="button" id="Add" value="">GET Weather</button>
+        </div>
+    </main>
+    <script src="scripts/selectCity.js" defer></script>
+</body>
+</html>
